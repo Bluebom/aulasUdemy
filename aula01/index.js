636c6b759bcbd6a6fa4f48a1e117272bfe8fd860
Bluebom <Franklinhpf@hotmail.com>
2021-01-15 13:43:02 -0300

    15/01/2021

--- aula01/index.js
+++ aula01/index.js
@@ -1,11 +1,25 @@
 // Tipo: String
+console.log('//////////////');
 console.log('TIPO: string')
 let firstLog = 'Hello world!';
 console.log(firstLog);
 console.log(typeof(firstLog));
 // Tipo: Number
+console.log('//////////////');
 console.log('TIPO: number')
 let secondLog = 12345;
 console.log(secondLog);
 console.log(typeof(secondLog));
+// Tipo: bolean
+console.log('//////////////');
+console.log('TIPO: bolean');
+let thirdLog = false;
+console.log(thirdLog);
+console.log(typeof(thirdLog));
+// Tipo: object
+console.log('//////////////');
+console.log('TIPO: object');
+let fourthLog = {oi: 'hi'}; //or []
+console.log(fourthLog);
+console.log(typeof(fourthLog));
 
